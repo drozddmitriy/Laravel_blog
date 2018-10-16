d5e229390616cf1de9fa1a0ae7ede3db037349c7
drozddmitriy <drozd.dmitriy.etc@gmail.com>
2018-10-16 21:29:48 +0300

    property

--- app/Post.php
+++ app/Post.php
@@ -4,6 +4,15 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Post
+ * @package App
+ *
+ * @property string $title
+ * @property string $intro
+ * @property string $body
+ * @property string $alias
+ */
 class Post extends Model
 {
     protected $fillable = ['title', 'intro','body', 'alias'];
